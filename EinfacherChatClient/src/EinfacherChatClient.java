b4069bc0ac72ed182c4928b5beef8eaede3c6d38
JonasF84 <jonas-fischer84@gmx.de>
2015-08-24 16:53:31 +0200

    EinfacherChatClient Updatet

--- EinfacherChatClient/src/EinfacherChatClient.java
+++ EinfacherChatClient/src/EinfacherChatClient.java
@@ -6,7 +6,9 @@ import java.awt.event.*;
 
 public class EinfacherChatClient {
 	
+	JTextArea eingehend;
 	JTextField ausgehend;
+	BufferedReader reader;
 	PrintWriter writer;
 	Socket sock;
 
@@ -18,12 +20,24 @@ public class EinfacherChatClient {
 	private void los() {
 		JFrame frame = new JFrame("Einfacher Chat-Client");
 		JPanel hauptPanel = new JPanel();
+		eingehend = new JTextArea(15, 20);
+		eingehend.setLineWrap(true);
+		eingehend.setWrapStyleWord(true);
+		eingehend.setEditable(false);
+		JScrollPane fScroller = new JScrollPane(eingehend);
+		fScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		fScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		ausgehend = new JTextField(20);
 		JButton sendenButton = new JButton("Senden");
 		sendenButton.addActionListener(new SendenButtonListener());
+		hauptPanel.add(fScroller);
 		hauptPanel.add(ausgehend);
 		hauptPanel.add(sendenButton);
 		netzwerkEinrichten();
+		
+		Thread readerThread = new Thread(new EingehendReader());
+		readerThread.start();
+		
 		frame.getContentPane().add(BorderLayout.CENTER, hauptPanel);
 		frame.setSize(400, 500);
 		frame.setVisible(true);
@@ -33,6 +47,8 @@ public class EinfacherChatClient {
 	private void netzwerkEinrichten(){
 		try{
 			sock = new Socket("127.0.0.1", 8090);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+			reader = new BufferedReader(streamReader);
 			writer = new PrintWriter(sock.getOutputStream());
 			System.out.println("Netzwerkanbindung steht");
 		}
@@ -46,7 +62,8 @@ public class EinfacherChatClient {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			try{
-				writer.println(ausgehend.getText());
+				String message = ausgehend.getText();
+				writer.println(message);
 				writer.flush();
 			}
 			catch(Exception e){
@@ -57,5 +74,20 @@ public class EinfacherChatClient {
 
 		}
 		
+	}
+	
+	public class EingehendReader implements Runnable{
+		public void run(){
+			String nachricht;
+			try{
+				while ((nachricht = reader.readLine()) != null){
+					System.out.println("gelesen: " + nachricht);
+					eingehend.append(nachricht + "\n");
+				}
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 	}
 }
---
+++ EinfacherChatClient/src/SehrEinfacherChatServer.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class SehrEinfacherChatServer {
+	
+	ArrayList clientOutputFlows;
+	
+	public class ClientHandler implements Runnable{
+		BufferedReader reader;
+		Socket sock;
+		
+		public ClientHandler(Socket clientSocket){
+			try{
+				sock = clientSocket;
+				InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+				reader = new BufferedReader(isReader);
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		public void run(){
+			String nachricht;
+			try{
+				while((nachricht = reader.readLine()) != null){
+					System.out.println("gelesen: " + nachricht);
+					esAllenWeitersagen(nachricht);
+				}
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new SehrEinfacherChatServer().los();
+	}
+
+	public void los() {
+		clientOutputFlows = new ArrayList();
+		
+		try{
+			ServerSocket serverSock = new ServerSocket(8090);
+			
+			while(true){
+				Socket clientSocket = serverSock.accept();
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				clientOutputFlows.add(writer);
+				Thread t = new Thread(new ClientHandler(clientSocket));
+				t.start();
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void esAllenWeitersagen(String nachricht){
+		Iterator it = clientOutputFlows.iterator();
+		while(it.hasNext()){
+			try{
+				PrintWriter writer = (PrintWriter) it.next();
+				writer.println(nachricht);
+				writer.flush();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
